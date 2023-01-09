2b8ffc5adb24268e0e132845a9d6428b94c390d4
FNR <francisnolascoromero@gmail.com>
2023-01-09 15:37:41 +0100

    CADENAS

---
+++ src/Ejercicios/Ejercicio1.java
@@ -0,0 +1,23 @@
+package Ejercicios;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //creamos un array de string que aparezca: nombre apellidos direccion telefono.
+        String persona
+        //usamos un bucle mejorado aparezca en una linea cada uno de los valores
+        //nombre
+        //apellidos
+        //..
+        // un metodo que pasemos el array anterior y devuelva un string como una linea
+        //"nombre, apellidos, direccion, telefono"
+        //mostramos en consola la cadena anterior
+        //pasamos la cadena anterior a un metodo y nos devuelve una nueva cadena
+        //pero esta sin espacios y en mayusculas
+        //"NOMBRE,APELLIDOS,DIRECCION,TELEFONO"
+        //Un metodo,que se le pase a la cadena anterior y nos devuelva el nombre
+        //con el formato apellidos, nombre
+        //un metodo que se le pase el string anterior de apellidos y nombre,nos diga cuantas
+        //vocales tiene entre apellidos y nombre
+    }
+}
+
