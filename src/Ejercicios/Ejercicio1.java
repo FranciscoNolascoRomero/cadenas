c31b9aa9284e293fd2920f6015e751ffb463824c
FNR <francisnolascoromero@gmail.com>
2023-01-10 15:14:22 +0100

    CADENAS

--- src/Ejercicios/Ejercicio1.java
+++ src/Ejercicios/Ejercicio1.java
@@ -5,68 +5,66 @@ import java.util.Arrays;
 
 public class Ejercicio1 {
     public static void main(String[] args) {
-        //creamos un array de string que aparezca: nombre apellidos direccion telefono.
-        String telefono="6891872342";
-        String nombre="Francisco";
-        String apellidos="Nolasco Romero";
-        String direccion="alonso de freylas";
-        String[] persona ={nombre,apellidos,direccion,telefono};
-        //usamos un bucle mejorado aparezca en una linea cada uno de los valores
-        for (String personas : persona) {
-            System.out.println(personas);
-        }
+        //creamos un array de String que aparezca: nombre, apellidos, dirección, telefono
+        /*String[] datos = new String[4];
+        datos[0] = "javier";
+        .....
+         */
+        String[] datos = {"javier", "gómez hernández", "avda. madrid 35", "953222222"};
+        //usamos un bucle mejorado aparezca en una línea cada uno de los valores:
         //nombre
         //apellidos
-        //..
-        // un metodo que pasemos el array anterior y devuelva un string como una lineas
-        System.out.println(nombre.concat(",").concat(apellidos).concat(",").concat(direccion).concat(",").concat(telefono));
-
+        //...
+        for ( String dato : datos ) {
+            System.out.println(dato);
+        }
+        //Un método que pasemos el array anterior y devuelva un String como una línea CSV
         //"nombre, apellidos, direccion, telefono"
-        //mostramos en consola la cadena anterior
-        //pasamos la cadena anterior a un metodo y nos devuelve una nueva cadena
-        //pero esta sin espacios y en mayusculas
-        //"NOMBRE,APELLIDOS,DIRECCION,TELEFONO"
+        String csv1 = crearCampoCSV(datos);
+        //mostramos en consola la cadena anterior, pero en mayúscula
+        System.out.printf("CSV: %S%n", csv1);
+        //Pasamos la cadena anterior a un método y nos devuelva una nueva cadena
+        //pero sin espacios y en mayúscula:
+        //"NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO"
+        String csv2 = crearCampoCSVSinEspaciosYMayuscula(csv1);
+        System.out.println(csv2);
 
-        System.out.println(nombre.concat(",").concat(apellidos).concat(",").concat(direccion).concat(",").concat(telefono).toUpperCase());
-
-        //Un metodo,que se le pase a la cadena anterior y nos devuelva el nombre
-        //con el formato apellidos, nombre
-        System.out.println(apellidos.concat(",").concat(nombre));
-        //un metodo que se le pase el string anterior de apellidos y nombre,nos diga cuantas
-        //vocales tiene entre apellidos y nombre
-        int vocales=0;
-        for (int i = 0; i < apellidos.length(); i++) {
-
-            char vocala = 'a';
-            char vocale = 'i';
-            char vocali = 'e';
-            char vocalo= 'o';
-            char vocalu = 'u';
-            char letra = apellidos.charAt(i);
-            if (letra == vocala || letra == vocali){
-                vocales++;
-            }
-            if (letra == vocale ||letra == vocalo ||letra == vocalu)
-                vocales++;
-        }
-        System.out.println("Apellidos tiene " + vocales + " vocales");
-        vocales=0;
-        for (int i = 0; i < nombre.length(); i++) {
-
-            char vocala = 'a';
-            char vocale = 'i';
-            char vocali = 'e';
-            char vocalo= 'o';
-            char vocalu = 'u';
-            char letra = nombre.charAt(i);
-            if (letra == vocala || letra == vocali){
-                vocales++;
-            }
-            if (letra == vocale ||letra == vocalo ||letra == vocalu)
-                vocales++;
-        }
-        System.out.println("Nombre tiene " + vocales + " vocales");
+        String nombre = crearNombre(csv2);
+        System.out.printf(nombre);
 
     }
+
+    private static String crearNombre(String csv2) {
+        //JAVIER,GÓMEZHERNÁNDEZ,AVDA.MADRID35,953222222
+        String[] csv22 = csv2.split(",");
+        return csv22[1].concat(",").concat(csv22[0]).toLowerCase();
+
+    }
+
+    private static String crearCampoCSVSinEspaciosYMayuscula(String csv1) {
+        csv1 = "JAVIER, GÓMEZ HERNÁNDEZ, AVDA. MADRID 35, 953222222".replaceAll("  ","");
+        return csv1.toUpperCase();
+    }
+
+    private static String crearCampoCSV(String[] datos) {
+        String csv = "";
+        for (String dato : datos) {
+            csv += dato + ", ";
+        }
+        if (csv.isEmpty())
+            return csv;
+        // else
+        return csv.substring(0, csv.length() - 2);
+        //javier,_gómez hernández,_avda. madrid 22,_953222222,_
+        //012.................................................csv.length-1
+        //substring(0, csv.length()-2)
+    }
+
+
+
+    //Un método, que se le pase la cadena anterior y nos devuelva el nombre
+    //con el formato apellidos, nombre
+    //Un método que se le pase el String anterior de apellidos y nombre, nos diga
+    //cuantas vocales tiene entre apellidos y nombre
 }
 
