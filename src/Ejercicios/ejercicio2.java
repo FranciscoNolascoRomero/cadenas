8f6f1035877dd18fb4662a03be1c89e6ecfd41cf
francisco nolasco <francisnolascoromero@gmail.com>
2023-01-12 20:51:45 +0100

    ejercicio casaa

--- src/Ejercicios/ejercicio2.java
+++ src/Ejercicios/ejercicio2.java
@@ -1,9 +1,8 @@
 package Ejercicios;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class ejercicio2 {
+public class  ejercicio2 {
     public static void main(String[] args) {
 //        creamos un list de string que aparezca:nombre, apellidos, direccoion, telefono
 //        List<String> Lista = new ArrayList<>();//lista que se puede modificar
@@ -20,29 +19,70 @@ public class ejercicio2 {
        }
         //Un método que pasemos la lista anterior y devuelva un String como una línea CSV
         //"nombre, apellidos, direccion, telefono" trabajando con string builder
-        String csv1 = crearCampoCSV1(Listas.toString());
+
+       String csv1 = crearCampoCSV1(Listas);
         System.out.println(csv1);
         //mostramos en consola la cadena anterior, pero en mayúscula
         //Pasamos la cadena anterior a un método y nos devuelva una nueva cadena
         //pero sin espacios y en mayúscula:
         //"NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO" USANDO STRINGBUILDER
+
+        String csv2 = crearCampoCSV1mayusculas(csv1);
+        System.out.println(csv2);
+
         //Un método, que se le pase la cadena anterior y nos devuelva el nombre
         //con el formato apellidos, nombre USANDO UN STRINGBUILDER
+        StringBuilder csv3 = mostrarNombre(Listas);
+        System.out.println(csv3);
         //un metodo que se le pasa la cadena  "NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO"
         // y un codigo postal devuelva "NOMBRE,APELLIDOS,DIRECCIÓN,TELÉFONO,CP" USANDO EL STRING BUILDER
+        String csv4 = añairCodigopostal(Listas);
+        System.out.println(csv4);
 
     }
 
-    private static String crearCampoCSV1(String Listas) {
-//        StringBuilder sBuilder = new StringBuilder();
-//        sBuilder.insert(1,Listas , 0,4);
-//       String csv1 = "";
-//        for (String Lista : sBuilder) {
-//           csv1 += apend + ", ";
-//       }
-//       if (csv1.isEmpty())
-//            return csv1;
+    private static String añairCodigopostal(List<String> Listas) {
+        StringBuilder sBuilder = new StringBuilder();
+
+        String csv = String.valueOf(sBuilder);
+        for (int i = 0; i < Listas.size(); i++) {
+            sBuilder.append(Listas.get(i) + ", ");
+        }
+        sBuilder.append("23002" + ", ");
+
         // else
-        return null;//sBuilder.substring(0, sBuilder.length() - 2);
+        return sBuilder.substring(0, sBuilder.length() - 2);
+    }
+
+    private static StringBuilder mostrarNombre(List<String> Listas) {
+        StringBuilder sBuilder = new StringBuilder();
+         StringBuilder csv =  sBuilder.append(Listas.get(1) + ", " + Listas.get(0));
+
+        return csv;
+    }
+
+    private static String crearCampoCSV1mayusculas(String csv1) {
+
+        StringBuilder sBuilder = new StringBuilder();
+      // String csv3= csv1.substring();
+       // sBuilder.replace(", ",",");
+
+        //csv = "nombre, apellidos, direccion, telefono"
+        //return NOMBRE,APELLIDOS,DIRECCION,TELEFONO
+        return null;
+        //falla por que quita el espacios en blanco de los apellidos
+        //solucionamos con split(", ")
+    }
+
+    private static String crearCampoCSV1(List<String> Listas) {
+        StringBuilder sBuilder = new StringBuilder();
+
+        String csv = String.valueOf(sBuilder);
+        for (int i = 0; i < Listas.size(); i++) {
+            sBuilder.append(Listas.get(i) + ", ");
+        }
+
+        // else
+        return sBuilder.substring(0, sBuilder.length() - 2);
     }
 }
